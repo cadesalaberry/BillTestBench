9118a6c30528de436553505fb20d2bb33f0c06d5
C-A de Salaberry <cadesalaberry@yahoo.com>
2014-10-23 21:43:26 -0400

    Add: Testing inPreparation.

---
+++ src/bill/BillTest.java
@@ -0,0 +1,158 @@
+package bill;
+
+import static org.junit.Assert.assertEquals;
+
+import java.lang.reflect.Field;
+
+import org.junit.Test;
+
+import bill.Bill.BillState;
+import bill.Bill.BillStateInHouseOfCommons;
+import bill.Bill.BillStateInSenate;
+
+public class BillTest {
+
+	/**
+	 * 1. inPreparation test 1
+	 */
+	@Test
+	public void testInPreparationIntroduceInHouse() {
+
+		Bill b = new Bill();
+
+		b.introduceInHouse();
+		assertEquals(BillState.inHouseOfCommons, getBillState(b));
+		assertEquals(BillStateInHouseOfCommons.firstReading,
+				getBillStateInHouseOfCommons(b));
+	}
+
+	/**
+	 * 1. inPreparation test 2
+	 */
+	@Test
+	public void testInPreparationIntroduceInSenate() {
+
+		Bill b = new Bill();
+
+		b.introduceInSenate();
+		assertEquals(BillState.inSenate, getBillState(b));
+		assertEquals(BillStateInSenate.firstReadingS, getBillStateInSenate(b));
+
+		assertEquals(false, isCommonBill(b));
+	}
+
+	/**
+	 * 1. inPreparation test 3, 4, 5, 6 and 7
+	 */
+	@Test
+	public void testInPreparationSneakPaths() {
+
+		Bill b = new Bill();
+
+		BillState expected = Bill.BillState.inPreparation;
+
+		assertEquals(expected, getBillState(b));
+
+		b.withdraw();
+		assertEquals(expected, getBillState(b));
+
+		b.votePasses();
+		assertEquals(expected, getBillState(b));
+
+		b.modify();
+		assertEquals(expected, getBillState(b));
+
+		b.voteFails();
+		assertEquals(expected, getBillState(b));
+
+		b.royalAssent();
+		assertEquals(expected, getBillState(b));
+	}
+
+	/**
+	 * Uses reflection to avoid modifying the code. Gets the bill state.
+	 * 
+	 * @param b
+	 * @return null if not found
+	 */
+	private BillState getBillState(Bill b) {
+
+		try {
+
+			Field f;
+			f = b.getClass().getDeclaredField("billState");
+			f.setAccessible(true);
+			return (BillState) f.get(b);
+
+		} catch (NoSuchFieldException | SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	private BillStateInHouseOfCommons getBillStateInHouseOfCommons(Bill b) {
+
+		try {
+
+			Field f;
+			f = b.getClass().getDeclaredField("billStateInHouseOfCommons");
+			f.setAccessible(true);
+			return (BillStateInHouseOfCommons) f.get(b);
+
+		} catch (NoSuchFieldException | SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	private BillStateInSenate getBillStateInSenate(Bill b) {
+
+		try {
+
+			Field f;
+			f = b.getClass().getDeclaredField("billStateInSenate");
+			f.setAccessible(true);
+			return (BillStateInSenate) f.get(b);
+
+		} catch (NoSuchFieldException | SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	private Boolean isCommonBill(Bill b) {
+
+		try {
+
+			Field f;
+			f = b.getClass().getDeclaredField("isCommonsBill");
+			f.setAccessible(true);
+			return (Boolean) f.get(b);
+
+		} catch (NoSuchFieldException | SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+}
